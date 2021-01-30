e8f2400c3031fc18af85d671f283165dc0f86a9f
Bharat Kumar <bkumarm048@gmail.com>
2021-01-30 14:06:57 +0530

    fix

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// contact us form submission sent to hosting provider
-document.querySelector("form").addEventListener("submit", handleSubmit);
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('pizzaOrder');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
-}
 
 // Preloader Function
 function loderfunction() {
