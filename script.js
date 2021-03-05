f0e5a5b3c0deadfca40252c8fa2eb51033b18712
Bharat Kumar <bkumarm048@gmail.com>
2021-03-06 00:52:54 +0530

    enquiry form tested

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const enquiryBtn = document.getElementById("enquiry-btn");
+const enquiryClose = document.getElementById("enquiry-close");
+const enquiryForm = document.getElementById("enquiry-form");
 
+enquiryBtn.addEventListener("click", (click) => {
+  enquiryForm.style.display = "block";
+  enquiryForm.style.right = 0;
+});
 
+enquiryClose.addEventListener("click", (click) => {
+  enquiryForm.style.display = "none";
+  enquiryForm.style.right = "-350px";
+});
 // Preloader Function
 function loderfunction() {
   document.getElementById("loading").style.display = "none";
